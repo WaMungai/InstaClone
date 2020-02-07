51aae5a02c32b347d30fea26e7d5533004eff2d7
Joyce <wamungai29@gmail.com>
2020-02-07 08:58:03 +0300

    Call the render function and pass in the request then template file.

--- insta/views.py
+++ insta/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Capture and share the worlds moments')
+    return render(request,'welcome.html')
