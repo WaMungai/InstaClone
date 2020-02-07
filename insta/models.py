ede0acde0a16b787051b819b1c4292e3d2499ffd
Joyce <wamungai29@gmail.com>
2020-02-07 10:59:59 +0300

    Implement media configurations settings in project.

--- insta/models.py
+++ insta/models.py
@@ -3,3 +3,8 @@ from django.contrib.auth.models import User
 from tinymce.models import HTMLField
 
 # Create your models here.
+class Profile(models.Model):
+    '''
+    Class containing user profile details
+    '''
+    profie_photo=models
