1cc86bb917c91ea7b5bba37fbfa71a5e1b03482b
Joyce <wamungai29@gmail.com>
2020-02-07 20:22:02 +0300

    Create view function that will be able to handle logic for displaying results.

--- insta/views.py
+++ insta/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 def welcome(request):
     return render(request,'welcome.html')
+
+def search_category(request):
+    if 'category' in request.GET and request.GET["category"]:
+        search_term=request.GET.get("category")
+        searched_categories=Image.search_by_category(search_term)
+        message=f"{search_term}"
+        return render(request,"search.html","message":message,"categories":searched_categories)
+    else:
+        message="You haven't searched for any term"
+        return render(request,'search.html',{"message":message})
