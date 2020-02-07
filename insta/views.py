e647b32e78fb981aed17eb65293feeaa36a3055d
Joyce <wamungai29@gmail.com>
2020-02-07 08:14:23 +0300

    Create welcome view function.

--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def welcome(request):
+    return HttpResponse('Capture and share the worlds moments')
