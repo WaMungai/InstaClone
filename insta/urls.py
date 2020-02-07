444a43dec8e421ac0aa03bb70e305023f6b82cf8
Joyce <wamungai29@gmail.com>
2020-02-07 08:45:17 +0300

    Define application configurations for template files.

--- insta/urls.py
+++ insta/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from . import views
 
-urlpatterns-[
+urlpatterns=[
     url('^$', views.welcome,name='welcome'),
 ]
--- instagram/settings.py
+++ instagram/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'insta',
+    'insta.apps.InstaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
