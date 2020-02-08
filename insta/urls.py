d4fc2b42623277ad0c1ab4ef6b017f39984cc971
Joyce <wamungai29@gmail.com>
2020-02-08 22:55:56 +0300

    Create a URLConf for the new image view.

--- insta/urls.py
+++ insta/urls.py
@@ -7,6 +7,7 @@ urlpatterns=[
     url('^$', views.welcome,name='welcome'),
     url('^search/', views.search_category,name='search_category'),
     url(r'photo/(\d+)',views.single_photo,name='photo'),
+    url(r'new/image$',views.new_image,name='new-image'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL,document_root =settings.MEDIA_ROOT)
