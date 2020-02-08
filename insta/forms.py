bcbb38d2f8b28e968de995f3f03c92a9f3dfe836
Joyce <wamungai29@gmail.com>
2020-02-08 13:59:33 +0300

    Create form class.

--- insta/forms.py
+++ insta/forms.py
@@ -1,1 +1,6 @@
 from django import forms
+from .models import Image,Profile,Comments
+
+class NewsLetterForm(forms.Form):
+    your_name=forms.CharField(label='First Name',max_length=30)
+    email=forms.EmailField(label='Email')
