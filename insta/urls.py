7c33e73c774b7bcb9a7abc8130ef6a30c1e9947b
Joyce <wamungai29@gmail.com>
2020-02-09 04:20:55 +0300

    Create URLConf for the updateprofile.

--- insta/urls.py
+++ insta/urls.py
@@ -9,6 +9,7 @@ urlpatterns=[
     url(r'photo/(\d+)',views.single_photo,name='photo'),
     url(r'new/image$',views.new_image,name='new-image'),
     url(r'new/profile$',view.new_profile,name='new-profile'),
+    url(r'updateprofile$',views.updateprofile,name='updateprofile'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL,document_root =settings.MEDIA_ROOT)
