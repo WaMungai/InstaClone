cadc0acbf02ffd0e4a97a717b7869e9011d44382
Joyce <wamungai29@gmail.com>
2020-02-09 04:50:11 +0300

    Create URLConf for comments.

--- insta/urls.py
+++ insta/urls.py
@@ -6,10 +6,11 @@ from . import views
 urlpatterns=[
     url('^$', views.welcome,name='welcome'),
     url('^search/', views.search_category,name='search_category'),
-    url(r'photo/(\d+)',views.single_photo,name='photo'),
-    url(r'new/image$',views.new_image,name='new-image'),
-    url(r'new/profile$',view.new_profile,name='new-profile'),
-    url(r'updateprofile$',views.updateprofile,name='updateprofile'),
+    url(r'^photo/(\d+)',views.single_photo,name='photo'),
+    url(r'^new/image$',views.new_image,name='new-image'),
+    url(r'^new/profile$',view.new_profile,name='new-profile'),
+    url(r'^updateprofile$',views.updateprofile,name='updateprofile'),
+    url(r'^comment$'.view.makecomment,name='makecomment'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL,document_root =settings.MEDIA_ROOT)
