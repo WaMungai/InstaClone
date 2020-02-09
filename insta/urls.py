72e078ddd05a2eecc7765d256ff589e5cf4030c3
Joyce <wamungai29@gmail.com>
2020-02-09 04:02:12 +0300

    Create a URLConf for the new profile.

--- insta/urls.py
+++ insta/urls.py
@@ -8,6 +8,7 @@ urlpatterns=[
     url('^search/', views.search_category,name='search_category'),
     url(r'photo/(\d+)',views.single_photo,name='photo'),
     url(r'new/image$',views.new_image,name='new-image'),
+    url(r'new/profile$',view.new_profile,name='new-profile'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL,document_root =settings.MEDIA_ROOT)
